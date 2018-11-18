60370d953914005fb92dd97fda69bb677fef6fe6
binau <aubin.roumillac@gmail.com>
2018-11-18 19:02:28 +0100

    plop

--- webworker/dist/worker1.js
+++
@@ -1,8 +0,0 @@
-import { Tools } from "./tools";
-onmessage = function (e) {
-    console.log('Worker: Message received from main script');
-    var workerResult = 'Result: ' + (e.data[0] * e.data[1]);
-    console.log('Worker: Posting message back to main script ' + Tools.calc(e.data[0], e.data[1]));
-    postMessage(workerResult);
-};
-//# sourceMappingURL=worker1.js.map
